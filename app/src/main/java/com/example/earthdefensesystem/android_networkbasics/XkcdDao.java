83fee075f6b5a37ffe4cefa959875dbf3e532e15
JTyzz <jtyzzer@gmail.com>
2018-12-05 15:56:56 -0800

    Jason Tyzzer SQL Implementation

---
+++ app/src/main/java/com/example/earthdefensesystem/android_networkbasics/XkcdDao.java
@@ -0,0 +1,11 @@
+package com.example.earthdefensesystem.android_networkbasics;
+
+public class XkcdDao {
+    public String baseUrl = "https://xkcd.com/";
+    public String jsonUrl = "info.0.json";
+    public String comicURL = "https://xkcd.com/info.0.json";
+
+    private static getComic (String url){
+        
+    }
+}
