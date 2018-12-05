1933f5786fd28d5aadc4c4c210070843bf0a171e
Patrick Martin <patrickmartin88@gmail.com>
2018-12-05 15:46:01 -0800

    Botton Navigation Broke. Fixing issue.

--- app/src/main/java/com/example/patrickjmartin/xkcd_persistance/MainActivity.java
+++ app/src/main/java/com/example/patrickjmartin/xkcd_persistance/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.patrickjmartin.xkcd_persistance;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import android.support.design.widget.BottomNavigationView;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView comImage;
+    private TextView comTitle, comAlt;
+
+    private XkcdComic prevComic;
+    private XkcdComic currentComic;
+    private XkcdComic nextComic;
+    private XkcdComic recentXkcdComic;
+    private BottomNavigationView navigation;
+    private int newestCom;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
