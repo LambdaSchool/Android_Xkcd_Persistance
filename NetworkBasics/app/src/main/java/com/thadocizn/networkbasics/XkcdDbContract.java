7d36d0c7b971ca253589e6e6ffb0af21c6c2546b
ThaDocIzN <rodan72@msn.com>
2018-12-05 18:56:45 -0600

    finished part 2

--- NetworkBasics/app/src/main/java/com/thadocizn/networkbasics/XkcdDbContract.java
+++ NetworkBasics/app/src/main/java/com/thadocizn/networkbasics/XkcdDbContract.java
@@ -1,5 +1,24 @@
 package com.thadocizn.networkbasics;
 
+import android.provider.BaseColumns;
+
+import org.w3c.dom.Text;
+
 public class XkcdDbContract {
+    public static class ComicEntry implements BaseColumns{
+        public static final String TABLE_NAME = "comics";
+
+        public static final String COLUMN_NAME_TIMESTAMP = "comic_timestamp";
+        public static final String COLUMN_NAME_BOOL = "comic_bool";
+
+        public static final String SQL_CREATE_TABLE = "CREATE TABLE " + TABLE_NAME +
+                " ( " +
+                _ID + " INTEGER PRIMARY KEY, " +
+                COLUMN_NAME_TIMESTAMP + " INTEGER, " +
+                COLUMN_NAME_BOOL + " INTEGER);";
+
+        public static final String SQL_DELETE_TABLE = "DROP TABLE IF EXISTS " +
+                TABLE_NAME + ";";
+    }
 
 }
