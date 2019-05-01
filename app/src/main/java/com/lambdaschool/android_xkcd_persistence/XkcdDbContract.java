6569ad392ce0d1bea7d73bace23717b84a11ab79
heiligbasil <basilief@yahoo.com>
2019-05-01 13:23:37 -0700

    Added DbContract class

---
+++ app/src/main/java/com/lambdaschool/android_xkcd_persistence/XkcdDbContract.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.android_xkcd_persistence;
+
+import android.provider.BaseColumns;
+
+public class XkcdDbContract {
+    public static class ComicEntry implements BaseColumns {
+        public static final String TABLE_NAME = "comic";
+        public static final String COLUMN_NAME_TIMESTAMP = "timestamp";
+        public static final String COLUMN_NAME_VIEWED = "viewed";
+
+        public static final String SQL_CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" + _ID + " INTEGER, " + COLUMN_NAME_TIMESTAMP + " INTEGER, " + COLUMN_NAME_VIEWED + " INTEGER);";
+
+        public static final String SQL_DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
+    }
+}
